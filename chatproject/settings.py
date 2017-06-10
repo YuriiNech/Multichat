f5efdaf775584fc4740ab4676dd353187e1716cf
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-10 22:24:50 +0300

    Create settings.py
    changed DB config

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -128,9 +128,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
@@ -143,12 +140,13 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-
-DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['multichat5.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
