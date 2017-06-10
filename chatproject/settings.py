d39dc9031ac5b60d8b0ab59b542f843207cf2a5d
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-10 23:39:27 +0300

    Create settings.py
    local_settings???

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -140,17 +140,17 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-# DATABASES['default'] = dj_database_url.config()
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = True
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
