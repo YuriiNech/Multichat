143b25bcfcb2f8f73bac6147a91be0cb5f924dba
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-13 18:25:06 +0300

    Create settings.py

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -50,11 +50,26 @@ INSTALLED_APPS = [
 #     },
 # }
 
+# import os
+# import urlparse
+
+# redis_url = urlparse.urlparse(os.environ.get('REDIS_URL'))
+# CACHES = {
+#     "default": {
+#          "BACKEND": "redis_cache.RedisCache",
+#          "LOCATION": "{0}:{1}".format(redis_url.hostname, redis_url.port),
+#          "OPTIONS": {
+#              "PASSWORD": redis_url.password,
+#              "DB": 0,
+#          }
+#     }
+# }
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'asgi_redis.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)],'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         'ROUTING': 'chatproject.routing.channel_routing',
     },
