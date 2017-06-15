f70d79a93fed23473c9db6b3c700bf6a19a1b99c
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-15 17:03:20 +0300

    Create consumers.py

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User, Group
 from channels.channel import Group, Channel
 from channels.auth import channel_session_user_from_http, channel_session_user
 import sqlite3
-import logging
-log = logging.getLogger(__name__)
+# import logging
+# log = logging.getLogger(__name__)
 
 @channel_session_user_from_http
 def ws_connect(message):
-    log.debug('now in cconsumers !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+#     log.debug('now in cconsumers !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     # accept reply_channel
     message.reply_channel.send({'accept': True})
 
