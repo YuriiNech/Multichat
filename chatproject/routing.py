f222dec9024deae3a950bb9c1ed0c3d3f766755c
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-15 22:40:52 +0300

    Update routing.py

--- chatproject/routing.py
+++ chatproject/routing.py
@@ -1,6 +1,6 @@
 # from channels.routing import route
 from channels.staticfiles import StaticFilesConsumer
-from .chatapp import consumers
+from chatapp import consumers
 print("in routing.py _______1________1__________1___________1________")
 channel_routing ={
     'http.request': StaticFilesConsumer(),
