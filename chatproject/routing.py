4a6e41c262f0b1bb7bfac77a714fdf2043c9c7c4
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-15 23:34:12 +0300

    Create routing.py

--- chatproject/routing.py
+++ chatproject/routing.py
@@ -1,9 +1,9 @@
-from channels.routing import route
-# from channels.staticfiles import StaticFilesConsumer
+# from channels.routing import route
+from channels.staticfiles import StaticFilesConsumer
 # from chatapp import consumers
 print("in routing.py _______1________1__________1___________1________")
 channel_routing ={
-#     'http.request': StaticFilesConsumer(),
+    'http.request': StaticFilesConsumer(),
 
     'websocket.connect': 'chatapp.consumers.ws_connect',
     'websocket.receive': 'chatapp.consumers.ws_message',
