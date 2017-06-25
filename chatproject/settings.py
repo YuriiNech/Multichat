1732bd2016fb43f5b157e37bfbadd2073d5c320a
Yurii Nechytailo <jurij_N@rambler.ru>
2017-06-25 21:52:56 +0300

    changed settings.py

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
-print("in settings.py _________________________________________")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -180,3 +179,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jurijn1961@gmail.com'
 EMAIL_HOST_PASSWORD = 'PoiuytrE'
 EMAIL_PORT = 587
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
