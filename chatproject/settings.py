d532abf5c859c344bf18899a0f7232c4058bd62c
Yurii Nechytailo <jurij_n@rambler.ru>
2018-08-22 09:31:45 +0000

    Changed settings.py

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -149,7 +149,7 @@ DEFAULT_FROM_EMAIL = 'jjurijn1961@gmail.com'  #'jurij_n@meta.ua'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'  #'smtp.meta.ua'
 EMAIL_HOST_USER = 'jn1961@gmail.com' # 'jurij_n@meta.uajuri'
-EMAIL_HOST_PASSWORD = '********************'
+EMAIL_HOST_PASSWORD = 'JuriJ&NataliA61'
 EMAIL_PORT = 587 # 465
 
 # try:
